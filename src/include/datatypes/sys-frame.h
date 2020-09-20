hostilefork/rebol

Eliminate REB_P_RETURN parameter class
Initial design of the definitional return mechanism used a special
parameter class that was identified during the frame filling in order
to preload it with a special ACTION! variant of RETURN which was
bound to the executing frame.

This led to some amount of inefficiency, in the sense that natives did
not actually use these RETURN cells (they only used the typeset in the
frame key for listing return types, and for type checking in the
debug build).  It also raised semantic questions about how a feature
that was intended to be something users could do themselves was doing
magic that function compositions couldn't obviously do.

But the system firmed up with the role of dispatchers and natives, and
the position of the return on functions with ACTION_FLAG_HAS_RETURN
standardized to the first argument slot (despite that slot being
what looked like the "first argument" prior to better locals handling).
This makes it fairly clear that no parameter class should be needed...
common code for the dispatchers can fill the slot.

(This may even permit phases to re-engineer the value to make the
return checks apply to the specific phase that's running.)

src/include/datatypes/sys-frame.h
@@ -945,14 +945,25 @@ inline static REB_R Init_Continuation_With_Core(
 
 
 // Common behavior shared by dispatchers which execute on BLOCK!s of code.
+// Assumes the code to be run is in the first details slot, and is relative
+// to the varlist of the frame.
 //
 inline static REB_R Interpreted_Continuation_Core(
     REBFRM *f,
     REBFLGS flags
 ){
-    REBARR *details = ACT_DETAILS(FRM_PHASE(f));
+    REBACT *phase = FRM_PHASE(f);
+    REBARR *details = ACT_DETAILS(phase);
     RELVAL *body = ARR_HEAD(details);  // usually CONST (doesn't have to be)
     assert(IS_BLOCK(body) and IS_RELATIVE(body) and VAL_INDEX(body) == 0);
+
+    if (GET_ACTION_FLAG(phase, HAS_RETURN)) {
+        assert(VAL_PARAM_SYM(ACT_PARAMS_HEAD(phase)) == SYM_RETURN);
+        REBVAL *cell = FRM_ARG(f, 1);
+        Move_Value(cell, NAT_VALUE(return));
+        INIT_BINDING(cell, f->varlist);
+        SET_CELL_FLAG(cell, ARG_MARKED_CHECKED);  // necessary?
+    }
 
     // The function body contains relativized words, that point to the
     // paramlist but do not have an instance of an action to line them up

jhgorse/ren-c

src/include/datatypes/sys-frame.h
@@ -947,14 +947,25 @@ inline static REB_R Init_Continuation_With_Core(
 
 
 // Common behavior shared by dispatchers which execute on BLOCK!s of code.
+// Assumes the code to be run is in the first details slot, and is relative
+// to the varlist of the frame.
 //
 inline static REB_R Interpreted_Continuation_Core(
     REBFRM *f,
     REBFLGS flags
 ){
-    REBARR *details = ACT_DETAILS(FRM_PHASE(f));
+    REBACT *phase = FRM_PHASE(f);
+    REBARR *details = ACT_DETAILS(phase);
     RELVAL *body = ARR_HEAD(details);  // usually CONST (doesn't have to be)
     assert(IS_BLOCK(body) and IS_RELATIVE(body) and VAL_INDEX(body) == 0);
+
+    if (GET_ACTION_FLAG(phase, HAS_RETURN)) {
+        assert(VAL_PARAM_SYM(ACT_PARAMS_HEAD(phase)) == SYM_RETURN);
+        REBVAL *cell = FRM_ARG(f, 1);
+        Move_Value(cell, NATIVE_VAL(return));
+        INIT_BINDING(cell, f->varlist);
+        SET_CELL_FLAG(cell, ARG_MARKED_CHECKED);  // necessary?
+    }
 
     // The function body contains relativized words, that point to the
     // paramlist but do not have an instance of an action to line them up